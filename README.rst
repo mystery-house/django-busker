2ebc0371d22608a835807ae2cc0b10b7ae5790aa
tinpan-io <moondog8@tilde.club>
2020-08-30 08:39:46 -0400

    Packaging files

--- README.rst
+++ README.rst
@@ -1,1 +1,5 @@
-# django-busker
+======
+Busker
+======
+Busker is a Django app for managing and redeeming codes that allow end-users to download digital assets
+(Music, E-Books, High-resolution artwork, etc.)
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    install_requires=[
+        'python-magic==0.4.18',
+        'django-markdownfield==0.8.0',
+        'django-queryset-csv==1.1.0',
+        'django-imagekit==4.0.2',
+    ]
+)
