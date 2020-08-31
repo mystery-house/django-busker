84dc654c5e9b5e577e29d9ee55a5f796e5669463
tinpan-io <moondog8@tilde.club>
2020-08-31 07:06:47 -0400

    Add Pillow to setup requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     install_requires=[
         'python-magic==0.4.18',
+        'Pillow==7.2.0',
         'django-markdownfield==0.8.0',
         'django-queryset-csv==1.1.0',
         'django-imagekit==4.0.2',
