caba0dbc7136efde3c91acb53f3a98e3f40f5979
tinpan-io <moondog8@tilde.club>
2020-09-07 11:48:52 -0400

    Add Django to package requirements

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     install_requires=[
         'python-magic==0.4.18',
         'Pillow==7.2.0',
+        'Django>=3.0',
         'django-markdownfield==0.8.0',
         'django-queryset-csv==1.1.0',
         'django-imagekit==4.0.2',
