2c6d9fd61db9476e7bbe8358b52e95ac666ecc42
tinpan-io <moondog8@tilde.club>
2020-09-07 11:56:59 -0400

    import cleanup

--- tests/urls.py
+++ tests/urls.py
@@ -16,10 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from django.conf import settings
-from django.conf.urls.static import static
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('busker-test/', include('busker.urls', namespace='busker')),
