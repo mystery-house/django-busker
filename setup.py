52df06b82e15572d0cd9c9d13ba2d5136585bc2d
Andy Chase <andychase@gmail.com>
2021-09-09 10:02:56 -0400

    Pillow security upgrade

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     install_requires=[
         'python-magic==0.4.24',
-        'Pillow==8.3.1',
+        'Pillow==8.3.2',
         'Django>=3.0',
         'django-markdownfield==0.10.0',
         'django-queryset-csv==1.1.0',
